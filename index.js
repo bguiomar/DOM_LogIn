0a0c39fbd24f959e3ad54cbc848977740ddeca2a
Blanca Guiomar Castro <bgcg1709@gmail.com>
2023-07-13 16:41:08 +0200

    done all

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 /*
   Exercises 01
   ------------
-  Add a label to each of the input fields: username, password, confirm password
+  Añadir un título para cada uno de los inputs: username, password, confirm password
 */
 document.getElementById('username').insertAdjacentHTML('beforeBegin','<label for="username"> Username </label>');
 
@@ -13,16 +13,20 @@ document.getElementById('confirmPassword').insertAdjacentHTML('beforeBegin','<la
 /*
   Exercise 02
   -----------
-  Add a required validation to each input that shows an error message next to the entry if it does not have any text entered.
+  Agregar una validación a cada input y que muestre un mensaje de error si no hay nada escrito dentro del input
 */
 
 const checkInput = (event) => {
-  const errorElement = Array.from(event.target.parentNode.querySelectorAll('span'));
-  
+      //con esta función lo que queremos es comprobar si existe en el código algún span. 
+  const errorElement = event.target.parentNode.querySelectorAll('span');
+
+// queremos comprobar que el input esté vacío o no, y que además no haya un error previo. Sabemos que no tenemos un error previo, al no tener un span. 
+
   if (event.target.value === '' && !errorElement.length){
   event.target.insertAdjacentHTML('afterEnd', '<span class="text-danger"> Required </span>')
   }
   
+  // esto lo tenemos por si hay un error previo y que hayamos escrito algo en el input (valu !== ''), entonces lo que hacemos es quitarlo, buscamos todos los elementos que son span y los eliminamos. OJO no volver a usar span
   if (errorElement && event.target.value !== '') { errorElement.forEach(elem => elem.remove());
  }
 };
@@ -34,25 +38,49 @@ document.getElementById('confirmPassword').addEventListener('blur',checkInput);
 /*
   Exercise 03
   -----------
-  Add a further validation to check if the user input in the password and confirm password inputs match.  Show an error message if they do not.
+  Añadir una validación que compruebe que los valores escritos en los inputs password y confirmPassword son iguales. En caso de ser diferentes, añadir un mesaje de error.
 */
-
+document.getElementById('confirmPassword').addEventListener('blur',(event)=>{
+    if (event.target.value !== document.getElementById('password').value){
+        event.target.insertAdjacentHTML('afterEnd', '<span class="text-danger">Passwords Should Match</span>');
+    }
+})
 
 
 /*
   Exercise 04
   -----------
-Ensure the ‘Register’ button is disabled until the user has entered valid data into all the input fields.  Once they have, the registration button should then be enabled.
+ Asegurarnos que el botón "Register" está por defecto deshabilitado hasta no haber rellenado todos los inputs. Una vez rellenados, se habilite. 
 */
 
+const btn= document.querySelector('button');
+btn.setAttribute('disabled','true') // con esto hacemos que esté deshabilitado.
+document.getElementById('registrationForm').addEventListener('change',(event)=>{ const formFilled = Array.from(document.querySelectorAll('input')).every(input => input.value);
+ if (formFilled){
+    btn.removeAttribute('disabled')
+ } else {
+    btn.setAttribute('disabled', 'true')
+ }
+
+})
 
 /*
   Exercise 05
   -----------
-When the user clicks the ‘Register’ button, a message should be displayed informing them of a successful user registration.
+  Cuando hagamos click en el boton "Register", salga un mensaje que nos informe que el registro se ha hecho satisfactoriamente. 
+  
 */
 
+const form = document.getElementById('registrationForm');
 
+form.addEventListener('submit', (event)=>{
+    event.preventDefault();
+    const alert = document.createElement('div');
+    alert.classList.add('alert','alert-success');
+    alert.innerText = 'user registered successfully';
+    form.prepend(alert)
+
+});
 
 
 
