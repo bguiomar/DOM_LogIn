2124b9bfc108ec98448481c1ca992dbd94628363
Guiomar Castro <99184600+bguiomar@users.noreply.github.com>
2023-07-13 19:07:04 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,11 +3,7 @@
   ------------
   Add a label to each of the input fields: username, password, confirm password
 */
-document.getElementById('username').insertAdjacentHTML('beforeBegin','<label for="username"> Username </label>');
 
-document.getElementById('password').insertAdjacentHTML('beforeBegin','<label for="password"> Password </label>');
-
-document.getElementById('confirmPassword').insertAdjacentHTML('beforeBegin','<label for="confirmPassword"> Confirm Password </label>');
 
 
 /*
@@ -16,20 +12,6 @@ document.getElementById('confirmPassword').insertAdjacentHTML('beforeBegin','<la
   Add a required validation to each input that shows an error message next to the entry if it does not have any text entered.
 */
 
-const checkInput = (event) => {
-  const errorElement = Array.from(event.target.parentNode.querySelectorAll('span'));
-  
-  if (event.target.value === '' && !errorElement.length){
-  event.target.insertAdjacentHTML('afterEnd', '<span class="text-danger"> Required </span>')
-  }
-  
-  if (errorElement && event.target.value !== '') { errorElement.forEach(elem => elem.remove());
- }
-};
-
-document.getElementById('username').addEventListener('blur',checkInput);
-document.getElementById('password').addEventListener('blur',checkInput);
-document.getElementById('confirmPassword').addEventListener('blur',checkInput);
 
 /*
   Exercise 03
